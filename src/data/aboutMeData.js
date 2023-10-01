97d831a5c89ca3c0bac6f511420c1f0b57bb5d9d
Neilsen Wei <neilsen.nngoi@gmail.com>
2023-10-02 11:32:24 +1300

    update projects with wawo

--- src/data/aboutMeData.js
+++ src/data/aboutMeData.js
@@ -1,10 +1,14 @@
 export const aboutMeData = [
   {
     id: 1,
-    bio: '',
+    bio: `A adventurer with a thirst to find out whats aroud the next corner, I jumped into web development and haven't looked back since
+    You'll usual find me mapping out a new location, or engaging with the culture of any location I find myself in, looking at the big picture 
+    while enjoying the little details`,
   },
   {
     id: 2,
-    bio: '',
+    bio: `A natural problem solver, I enjoy the challenge of solving issues to make life easier for everyone, I'm a team player and not afraid to
+    take on leadership roles. I've been told I bring good energy and vibes to group working enviornments. Attentive and goal oriented I don't skip
+    out on little details even as I focus on reaching deadlines`,
   },
 ]
