0bc15d0524ef67879e921e3388902c13a58b5c4d
Neilsen Wei <neilsen.nngoi@gmail.com>
2023-10-02 11:52:11 +1300

    update contact form with emailjs

--- src/data/aboutMeData.js
+++ src/data/aboutMeData.js
@@ -1,7 +1,7 @@
 export const aboutMeData = [
   {
     id: 1,
-    bio: `A adventurer with a thirst to find out whats aroud the next corner, I jumped into web development and haven't looked back since
+    bio: `An adventurer with a thirst to always discover whats aroud the next corner, I jumped into web development and haven't looked back since
     You'll usual find me mapping out a new location, or engaging with the culture of any location I find myself in, looking at the big picture 
     while enjoying the little details`,
   },
