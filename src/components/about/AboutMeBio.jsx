3b10263f4aad775907ca7efce279a46a2c2db875
Neilsen Wei <neilsen.nngoi@gmail.com>
2023-10-02 11:10:23 +1300

    update profile img

--- src/components/about/AboutMeBio.jsx
+++ src/components/about/AboutMeBio.jsx
@@ -1,4 +1,4 @@
-import profileImage from '../../images/profile.jpeg'
+import profileImage from '../../images/profile.jpg'
 import { useContext } from 'react'
 import AboutMeContext from '../../context/AboutMeContext'
 
