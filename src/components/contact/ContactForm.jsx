34cd7ad56b481cb81efb7914a67638e0e98d6a8b
Neilsen Wei <neilsen.nngoi@gmail.com>
2023-10-02 13:30:26 +1300

    comment out contact form

--- src/components/contact/ContactForm.jsx
+++ src/components/contact/ContactForm.jsx
@@ -1,99 +1,99 @@
-import { send } from 'vite'
-import Button from '../reusable/Button'
-import FormInput from '../reusable/FormInput'
-import emailjs from '@emailjs/browser'
-import React, { useRef } from 'react'
+// import { send } from 'vite'
+// import Button from '../reusable/Button'
+// import FormInput from '../reusable/FormInput'
+// import emailjs from '@emailjs/browser'
+// import React, { useRef } from 'react'
 
-const ContactForm = () => {
-  const form = useRef()
+// const ContactForm = () => {
+//   const form = useRef()
 
-  const sendEmail = (e) => {
-    e.preventDefault()
+//   const sendEmail = (e) => {
+//     e.preventDefault()
 
-    emailjs
-      .sendForm(
-        'service_mpy5nlr',
-        'template_fn1kjxw',
-        form.current,
-        'l2uyjepPaaca7uClf'
-      )
-      .then(
-        (result) => {
-          console.log(result.text)
-        },
-        (error) => {
-          console.log(error.text)
-        }
-      )
-  }
+//     emailjs
+//       .sendForm(
+//         'service_mpy5nlr',
+//         'template_fn1kjxw',
+//         form.current,
+//         'l2uyjepPaaca7uClf'
+//       )
+//       .then(
+//         (result) => {
+//           console.log(result.text)
+//         },
+//         (error) => {
+//           console.log(error.text)
+//         }
+//       )
+//   }
 
-  return (
-    <div className="w-full lg:w-1/2">
-      <div className="leading-loose">
-        <form
-          ref={form}
-          onSubmit={sendEmail}
-          className="max-w-xl m-4 p-6 sm:p-10 bg-secondary-light dark:bg-secondary-dark rounded-xl shadow-xl text-left"
-        >
-          <p className="font-general-medium text-primary-dark dark:text-primary-light text-2xl mb-8">
-            Contact Form
-          </p>
-          <FormInput
-            inputLabel="Full Name"
-            labelFor="name"
-            inputType="text"
-            inputId="name"
-            inputName="name"
-            placeholderText="Your Name"
-            ariaLabelName="Name"
-          />
-          <FormInput
-            inputLabel="Email"
-            labelFor="email"
-            inputType="email"
-            inputId="email"
-            inputName="email"
-            placeholderText="Your email"
-            ariaLabelName="Email"
-          />
-          <FormInput
-            inputLabel="Subject"
-            labelFor="subject"
-            inputType="text"
-            inputId="subject"
-            inputName="subject"
-            placeholderText="Subject"
-            ariaLabelName="Subject"
-          />
+//   return (
+//     <div className="w-full lg:w-1/2">
+//       <div className="leading-loose">
+//         <form
+//           ref={form}
+//           onSubmit={sendEmail}
+//           className="max-w-xl m-4 p-6 sm:p-10 bg-secondary-light dark:bg-secondary-dark rounded-xl shadow-xl text-left"
+//         >
+//           <p className="font-general-medium text-primary-dark dark:text-primary-light text-2xl mb-8">
+//             Contact Form
+//           </p>
+//           <FormInput
+//             inputLabel="Full Name"
+//             labelFor="name"
+//             inputType="text"
+//             inputId="name"
+//             inputName="name"
+//             placeholderText="Your Name"
+//             ariaLabelName="Name"
+//           />
+//           <FormInput
+//             inputLabel="Email"
+//             labelFor="email"
+//             inputType="email"
+//             inputId="email"
+//             inputName="email"
+//             placeholderText="Your email"
+//             ariaLabelName="Email"
+//           />
+//           <FormInput
+//             inputLabel="Subject"
+//             labelFor="subject"
+//             inputType="text"
+//             inputId="subject"
+//             inputName="subject"
+//             placeholderText="Subject"
+//             ariaLabelName="Subject"
+//           />
 
-          <div className="mt-6">
-            <label
-              className="block text-lg text-primary-dark dark:text-primary-light mb-2"
-              htmlFor="message"
-            >
-              Message
-            </label>
-            <textarea
-              className="w-full px-5 py-2 border border-gray-300 dark:border-primary-dark border-opacity-50 text-primary-dark dark:text-secondary-light bg-ternary-light dark:bg-ternary-dark rounded-md shadow-sm text-md"
-              id="message"
-              name="message"
-              cols="14"
-              rows="6"
-              aria-label="Message"
-            ></textarea>
-          </div>
+//           <div className="mt-6">
+//             <label
+//               className="block text-lg text-primary-dark dark:text-primary-light mb-2"
+//               htmlFor="message"
+//             >
+//               Message
+//             </label>
+//             <textarea
+//               className="w-full px-5 py-2 border border-gray-300 dark:border-primary-dark border-opacity-50 text-primary-dark dark:text-secondary-light bg-ternary-light dark:bg-ternary-dark rounded-md shadow-sm text-md"
+//               id="message"
+//               name="message"
+//               cols="14"
+//               rows="6"
+//               aria-label="Message"
+//             ></textarea>
+//           </div>
 
-          <div className="font-general-medium w-40 px-4 py-2.5 text-white text-center font-medium tracking-wider bg-indigo-500 hover:bg-indigo-600 focus:ring-1 focus:ring-indigo-900 rounded-lg mt-6 duration-500">
-            <Button
-              title="Send Message"
-              type="submit"
-              aria-label="Send Message"
-            />
-          </div>
-        </form>
-      </div>
-    </div>
-  )
-}
+//           <div className="font-general-medium w-40 px-4 py-2.5 text-white text-center font-medium tracking-wider bg-indigo-500 hover:bg-indigo-600 focus:ring-1 focus:ring-indigo-900 rounded-lg mt-6 duration-500">
+//             <Button
+//               title="Send Message"
+//               type="submit"
+//               aria-label="Send Message"
+//             />
+//           </div>
+//         </form>
+//       </div>
+//     </div>
+//   )
+// }
 
-export default ContactForm
+// export default ContactForm
