5f0103880ec6cc5e2d6400bfb71c304b49992532
Neilsen Wei <neilsen.nngoi@gmail.com>
2023-10-04 11:37:48 +1300

    update portfolio

--- src/components/shared/AppBanner.jsx
+++ src/components/shared/AppBanner.jsx
@@ -50,8 +50,8 @@ const AppBanner = () => {
           className="flex justify-center sm:block"
         >
           <a
-            download="neilsen-ngoi-2023.pdf"
-            href="/files/neilsen-ngoi-2023.pdf"
+            download="neilsen-ngoi.pdf"
+            href="/files/neilsen-ngoi.pdf"
             className="font-general-medium flex justify-center items-center w-36 sm:w-48 mt-12 mb-6 sm:mb-0 text-lg border border-indigo-200 dark:border-ternary-dark py-2.5 sm:py-3 shadow-lg rounded-lg bg-indigo-50 focus:ring-1 focus:ring-indigo-900 hover:bg-indigo-500 text-gray-500 hover:text-white duration-500"
             aria-label="Download Resume"
           >
